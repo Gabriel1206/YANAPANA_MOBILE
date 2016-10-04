7fcadcded920c5edb97bccc775b81234efd76d13
JuDaPalaciosTaboada <juanpalaciostaboada@gmail.com>
2016-10-04 16:50:44 -0500

    Nuevo menu, comunicacion con visit_activity

---
+++ Yanapan/app/src/main/java/pe/edu/upc/yanapan/LoginActivity.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.yanapan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button = (Button) findViewById(R.id.LogIn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+
+                String user = ((EditText) findViewById(R.id.c_name)).getText().toString();
+                String password = ((EditText) findViewById(R.id.c_password)).getText().toString();
+                if (user.equals("admin")&& password.equals("admin")) {
+
+                    Intent menu = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(menu);
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+        ;
+    }
+
+
+}
--- Yanapan/app/src/main/java/pe/edu/upc/yanapan/MenuActivity.java
+++ Yanapan/app/src/main/java/pe/edu/upc/yanapan/MenuActivity.java
@@ -5,23 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
-
-import android.support.design.widget.NavigationView;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ListView;
 
-import static android.R.attr.fragment;
-
-public class MenuActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MenuActivity extends AppCompatActivity {
+    ListView listView;
+    String[] valor = new String[] {"CheckIn - CheckOut","Visits" ,"Informs"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,67 +40,25 @@ public class MenuActivity extends AppCompatActivity
 
         imageView.setImageDrawable(roundedDrawable);
 
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        listView =(ListView) findViewById(R.id.list);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,valor);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if( position == 1){
+                    Intent visits = new Intent(view.getContext(),VisitActivity.class);
+                    startActivity(visits);
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.visits) {
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        boolean fragmentTransaction = false;
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-            return super.onOptionsItemSelected(item);
-
-
-        } else if (id == R.id.checkin) {
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-            return super.onOptionsItemSelected(item);
-
-        } else if (id == R.id.inform) {
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-        } else if (id == R.id.nav_share) {
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-        } else if (id == R.id.nav_send) {
-            Intent visits = new Intent(this, VisitActivity.class);
-            startActivity(visits);
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
 }
