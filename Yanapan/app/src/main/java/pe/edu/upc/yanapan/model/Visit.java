7989c9a42c9d09e4d1d0b9480c7aee611e40b666
Johan Muñoz Cavero <jmunnozc@gmail.com>
2016-10-10 21:29:41 -0500

    Grabacion y consumo del WS

---
+++ Yanapan/app/src/main/java/pe/edu/upc/yanapan/model/Visit.java
@@ -0,0 +1,59 @@
+package pe.edu.upc.yanapan.model;
+
+import java.util.List;
+
+public class Visit {
+
+	private int idVisit;
+	private String longitude;
+	private String latitude;
+	private List<DetailVisitBeneficiary> lstDetailVisitBeneficiary;
+	private User user;
+		
+	public Visit() {
+		super();
+	}
+	public int getIdVisit() {
+		return idVisit;
+	}
+	public void setIdVisit(int idVisit) {
+		this.idVisit = idVisit;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+			
+	public List<DetailVisitBeneficiary> getLstDetailVisitBeneficiary() {
+		return lstDetailVisitBeneficiary;
+	}
+	public void setLstDetailVisitBeneficiary(
+			List<DetailVisitBeneficiary> lstDetailVisitBeneficiary) {
+		this.lstDetailVisitBeneficiary = lstDetailVisitBeneficiary;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Override
+	public String toString() {
+		return "Visit [idVisit=" + idVisit + ", longitude=" + longitude
+				+ ", latitude=" + latitude + ", lstDetailVisitBeneficiary="
+				+ lstDetailVisitBeneficiary + ", user=" + user + "]";
+	}
+	
+	
+	
+		
+}
